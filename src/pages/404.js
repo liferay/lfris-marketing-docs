e55539466ad0fa405158d12ffd0e91c3e2397635
Justin Sun-ho Kim <justin.kim@liferay.com>
2019-06-28 14:38:00 -0700

    Rudimentary navigation on 404 page for prod

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import { withPrefix } from 'gatsby';
+import Sidebar from '../components/Sidebar';
 
-const ErrorPage = () => (
+const ErrorPage = ({ location }) => (
     <>
         <div className="container-fluid container-fluid-max-lg">
             <img className="image-fluid" alt="not found" src={withPrefix("images/ray.png")}></img>
             <h1 className="h1">Not Found</h1>
+
+            <Sidebar location={location} navbarToggled={ true } />
         </div>
     </>
 );
