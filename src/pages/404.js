b81769ba53f3d0cc84d1dcd8cb9c82a8002e9291
Justin Sun-ho Kim <justin.kim@liferay.com>
2019-06-28 14:38:00 -0700

    Rudimentary navigation on 404 page for prod

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,28 +1,16 @@
-import Footer from '../components/Footer';
-import LayoutNav from '../components/LayoutNav';
 import React from 'react';
-import Sidebar from '../components/Sidebar';
 import { withPrefix } from 'gatsby';
+import Sidebar from '../components/Sidebar';
 
 const ErrorPage = ({ location }) => (
-    <div className="error-page">
-        <main className="content">
-            <header className="header">
-                <LayoutNav effect={true} static={true} sidebarHamburguerIcon={true} />
-            </header>
+    <>
+        <div className="container-fluid container-fluid-max-lg">
+            <img className="image-fluid" alt="not found" src={withPrefix("images/ray.png")}></img>
+            <h1 className="h1">Not Found</h1>
 
-            <div className="container-fluid container-fluid-max-lg">
-                <img className="image-fluid" alt="not found" src={withPrefix("images/ray.png")}></img>
-                <h1 className="h1">Not Found</h1>
-
-                {/* <Sidebar location={location} navbarToggled={true} /> */}
-            </div>
-
-            <section>
-                <Footer />
-            </section>
-        </main>
-    </div>
+            <Sidebar location={location} navbarToggled={ true } />
+        </div>
+    </>
 );
 
 export default ErrorPage;
