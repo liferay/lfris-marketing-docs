671de6b99fb33ef9642ccba926df373bd50ef40c
Justin Sun-ho Kim <justin.kim@liferay.com>
2019-07-08 15:54:51 -0700

    LWM-1707 Move custom components to CSS Modules

--- src/components/LogoutContainer/LogoutContainer.js
+++ src/components/LogoutContainer/LogoutContainer.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import { Avatar, Collapse } from 'react-md'
+import styles from './styles.module.scss'
 
 export default class LogoutContainer extends Component {
 	state = { collapsed: true }
@@ -18,14 +19,14 @@ export default class LogoutContainer extends Component {
 		const { collapsed } = this.state
 
 		return (
-			<div class="logout-container-wrapper">
-				<Avatar className="logout-container-avatar" src={this.props.user.avatar} onClick={this.toggle} role="presentation" />
+			<div class={styles.logoutContainerWrapper}>
+				<Avatar className={styles.logoutContainerAvatar} src={this.props.user.avatar} onClick={this.toggle} role="presentation" />
 				<Collapse animate={false} collapsed={collapsed}>
 					<div className="caret"></div>
-					<div className="logout-container">
-						<h4 className="logout-user-name">{this.props.user.name}</h4>
-						<div className="logout-email-address">{this.props.user.email}</div>
-						<h5 className="logout-button mx-3"
+					<div className={styles.logoutContainer}>
+						<h4 className={styles.logoutUserName}>{this.props.user.name}</h4>
+						<div className={styles.logoutEmailAddress}>{this.props.user.email}</div>
+						<h5 className={`${styles.logoutButton} mx-3`}
 							onClick={this.props._handleLogout}>
 							Logout
 						</h5>
