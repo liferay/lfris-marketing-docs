406114edac0649378e49f8ebbc296ae6c25b30b3
Justin Sun-ho Kim <justin.kim@liferay.com>
2019-07-24 11:03:54 -0700

    .

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -5,13 +5,20 @@ import './styles.module.scss'
 
 const Login = () => {
   const identity = useIdentityContext()
-  const [dialog, setDialog] = React.useState(false)
 
   console.log(JSON.stringify(identity))
-  const isLoggedIn = identity && identity.isLoggedIn
+  const isLoggedIn = () => {
+    return  identity && identity.isLoggedIn;
+  }
+
+  netlifyIdentity.on('login', isLoggedIn());
+  netlifyIdentity.on('logout', isLoggedIn());
+
   return (
     <>
-        <button className="btn btn-sm btn-outline-light font-weight-bold mx-3" onClick={() => netlifyIdentity.open()}>
+        <button className="btn btn-sm btn-outline-light font-weight-bold mx-3" onClick={() => {
+          netlifyIdentity.open();
+        }}>
           {isLoggedIn ? `LOG OUT` : "LOG IN"}
         </button>
     </>
