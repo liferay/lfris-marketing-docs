adfa0afd07293683ec3f9e29d1a0fcbae6a9ec93
Justin Sun-ho Kim <justin.kim@liferay.com>
2019-08-26 16:11:01 -0700

    LWM-1885 Login improvements

--- src/services/auth.js
+++ src/services/auth.js
@@ -1,48 +1,51 @@
-import netlifyIdentity from 'netlify-identity-widget';
+import netlifyIdentity from "netlify-identity-widget"
 
 export const isBrowser = () => typeof window !== "undefined"
 
+if (isBrowser()) {
+    netlifyIdentity.init()
+    window.netlifyIdentity = netlifyIdentity
+}
+
+export const getUser = () =>
+  isBrowser() && window.localStorage.getItem("netlifyUser")
+    ? JSON.parse(window.localStorage.getItem("netlifyUser"))
+    : {}
+
+const setUser = user =>
+  window.localStorage.setItem("netlifyUser", JSON.stringify(user))
+
 export const handleLogin = () => {
-    return new Promise((resolve, reject) => {
-        if (isLoggedIn()) {
-            resolve();
-        } else {
-            netlifyIdentity.open();
-            netlifyIdentity.on('login', user => {
-                netlifyIdentity.close();
-                resolve();
-            });
-        }
-        netlifyIdentity.on('error', err => {
-            reject(err);
-        });
-    });
+  if (!isLoggedIn()) {
+    netlifyIdentity.open()
+    netlifyIdentity.on("login", user => {
+      setUser(user)
+    })
+  }
 }
 
 export const isLoggedIn = () => {
-    return !!netlifyIdentity.currentUser();
+  if (!isBrowser()) return false
+  const user = netlifyIdentity.currentUser()
+  return !!user
+}
+
+export const handleLogout = () => {
+  netlifyIdentity.logout()
+  netlifyIdentity.on("logout", () => {
+    setUser({})
+  })
 }
 
 export const getUserAuthentication = () => {
-    let isUserAuthenticated = false;
-
     if(isLoggedIn()) {
         const authenticatedUsers = ['liferay.com', 'triblio.com', 'kyrodigital.com'];
         const userEmail = netlifyIdentity.currentUser().email;
 
-        isUserAuthenticated = userEmail && authenticatedUsers.some(user => {
+        return authenticatedUsers.some(user => {
             return userEmail.includes(user);
         });
+    } else {
+        return false
     }
-
-    return isUserAuthenticated;
-}
-
-export const logout = () => {
-    return new Promise((resolve, reject) => {
-        netlifyIdentity.logout();
-        netlifyIdentity.on('logout', () => {
-            resolve();
-        });
-    });
 }
