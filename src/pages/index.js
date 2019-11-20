ec560b74e21a0c02ed074850cbf3923d6b92785b
Justin Sun-ho Kim <justin.kim@liferay.com>
2019-11-20 08:43:24 -0800

    LWM-2081 Create Layout Component for easier dev

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,41 +1,12 @@
-import Helmet from 'react-helmet';
 import React from 'react';
-import { Footer, LayoutNav } from 'components/organisms';
-import { withPrefix, Link, graphql } from 'gatsby';
+import { MainLayout } from 'components/layouts';
 
-const Index = ({ data }) =>  {    
-    const description = "Empowering Liferay Marketing";
-
+const Index = () =>  {    
     return (
-        <div className="home">
-            <Helmet>
-                <title>{process.env.PROJECT_NAME}</title>
-                <meta name="description" content={description} />
-                <meta name="og:description" content={description} />
-                <meta name="twitter:description" content={description} />
-                <meta name="og:title" content={process.env.PROJECT_NAME} />
-            </Helmet>
-            <main className="content">
-                <header className="header">
-                    <LayoutNav siteTitle={data.site.siteMetadata.title} search={true} />
-                </header>
-
-                <section>
-                    <Footer />
-                </section>
-            </main>
-        </div>
+        <MainLayout className='home'>
+            test
+        </MainLayout>
     );  
 };
-
-export const query = graphql`
-    query {
-        site {
-            siteMetadata {
-                title
-            }
-        }
-    }
-`
 
 export default Index;
