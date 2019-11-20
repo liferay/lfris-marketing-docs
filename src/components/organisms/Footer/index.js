17d6ccfb07791f3d2175624ba8d0692bac039825
Justin Sun-ho Kim <justin.kim@liferay.com>
2019-11-20 09:41:49 -0800

    LWM-2081 Style Footer

--- src/components/organisms/Footer/index.js
+++ src/components/organisms/Footer/index.js
@@ -1,23 +1,12 @@
 import React from 'react';
 import { withPrefix } from 'gatsby';
+import styles from './styles.module.scss'
 
 const Footer = () => {
     return (
-        <div className="footer">
-            <div className="container-fluid container-fluid-max-lg">
-                <div className="row">
-                    <div className="col-lg text-center text-lg-left mb-4 mb-lg-0">
-                        <img className="logo" alt="liferay logo" src={withPrefix("images/liferayLogo.png")} />
-
-                        <div className="clearfix d-lg-none"></div>
-
-                        Brought to you by <a href="http://www.liferay.com" className="font-weight-bold"  target="_blank" rel="noopener noreferrer">Liferay, Inc.</a>
-                    </div>
-
-                    <div className="col-lg text-center text-lg-right">
-                        <div>Powered by <a href="https://wedeploy.com/" className="font-weight-bold"  target="_blank" rel="noopener noreferrer">WeDeploy™</a></div>
-                    </div>
-                </div>
+        <div className={styles.footer}>
+            <div className={styles.footerText}>
+                © 2019 LIFERAY INC. ALL RIGHTS RESERVED
             </div>
         </div>
     );
