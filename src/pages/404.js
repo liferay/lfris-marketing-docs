851c7de51b322e5f32cb9c325388e83d317e8ec1
Justin Sun-ho Kim <justin.kim@liferay.com>
2019-11-20 09:06:22 -0800

    LWM-2081 Use Layout component in every page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,24 +1,14 @@
 import React from 'react';
 import styles from './styles.module.scss';
-import { Footer, LayoutNav } from 'components/organisms';
+import { MainLayout } from 'components/layouts'
 import { withPrefix } from 'gatsby';
 
 const ErrorPage = ({ location }) => (
-    <div className="error-page">
-        <main className="content">
-            <header className="header">
-                <LayoutNav effect={true} static={true} sidebarHamburguerIcon={true} />
-            </header>
-
-            <div className={`container-fluid container-fluid-max-lg ${styles.container}`}>
-                <img className={`image-fluid ${styles.img}`} alt="not found" src={withPrefix("images/404_Illustration.jpg")}></img>
-            </div>
-
-            <section className={styles.footerContainer}>
-                <Footer />
-            </section>
-        </main>
-    </div>
+    <MainLayout className="error-page">
+        <div className={`container-fluid container-fluid-max-lg ${styles.container}`}>
+            <img className={`image-fluid ${styles.img}`} alt="not found" src={withPrefix("images/404_Illustration.jpg")}></img>
+        </div>
+    </MainLayout>
 );
 
 export default ErrorPage;
