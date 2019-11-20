fd725facb2b62c3bcc2764a415e78b4c4e03c7ea
Justin Sun-ho Kim <justin.kim@liferay.com>
2019-11-20 11:11:01 -0800

    LWM-2081 Switch from clay to lrdcom theme

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,4 +1,4 @@
-require('./src/styles/main.scss');
+require('./src/theme/main.scss');
 
 // A stub function is needed because gatsby won't load this file otherwise
 // (https://github.com/gatsbyjs/gatsby/issues/6759)
