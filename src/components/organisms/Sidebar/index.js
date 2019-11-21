4393f32b5da633a6cd79c23e2cff326b5db44afd
Justin Sun-ho Kim <justin.kim@liferay.com>
2019-11-21 11:49:26 -0800

    LWM-2078 Convert Sidebar.js to class component (easier state)

--- src/components/organisms/Sidebar/index.js
+++ src/components/organisms/Sidebar/index.js
@@ -4,96 +4,114 @@ import Sidebarselect from './SidebarSelect';
 import React from 'react';
 import styles from './styles.module.scss';
 
-const getTree = data => {
-	let tree = [];
-	let paths = data.map(({node}) => {
-		const { fields: { slug } } = node;
+class Sidebar extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state= {
+			selectedValue: 0
+		}
 
-		return slug.startsWith('/') ? slug.slice(1).split('/') : slug.split('/');
-	});
+		this._getTree = this._getTree.bind(this);
+		this._findFolder = this._findFolder.bind(this);
+		this._selected = this._selected.bind(this);
+	}
 
-	paths.forEach(path => {
-		var currentLevel = tree;
-		
-        path.forEach((folder, index) => {	
-			var existingPath = findFolder(currentLevel, 'name', folder);
+	_getTree = (data, index) => {
+		let tree = [];
+		let paths = data.map(({node}) => {
+			const { fields: { slug } } = node;
 	
-			if (existingPath) {
-				currentLevel = existingPath.children;
-			} else {
-				var newFolder = {
-					children: [],
-					isFolder: index < path.length - 1,
-					link: '/' + path.join('/').replace(/.html|.md|.mdx/g, ''),
-					name: folder.replace(/.html|.md|.mdx/g, '')
-				};
-	
-				currentLevel.push(newFolder);
-				currentLevel = newFolder.children;
-			}
+			return slug.startsWith('/') ? slug.slice(1).split('/') : slug.split('/');
 		});
-	});
-	console.log(tree);
-	console.log(tree[0].children);
+	
+		paths.forEach(path => {
+			var currentLevel = tree;
+			
+			path.forEach((folder, index) => {	
+				var existingPath = this._findFolder(currentLevel, 'name', folder);
+		
+				if (existingPath) {
+					currentLevel = existingPath.children;
+				} else {
+					var newFolder = {
+						children: [],
+						isFolder: index < path.length - 1,
+						link: '/' + path.join('/').replace(/.html|.md|.mdx/g, ''),
+						name: folder.replace(/.html|.md|.mdx/g, '')
+					};
+		
+					currentLevel.push(newFolder);
+					currentLevel = newFolder.children;
+				}
+			});
+		});
+		console.log(tree);
+		console.log(tree[0].children);
+	
+		return tree;
+	};
+	
+	_findFolder(array, key, value) {
+		var t = 0;
+		while (t < array.length && array[t][key] !== value) { t++; };
+	
+		if (t < array.length) {
+			return array[t]
+		} else {
+			return false;
+		}
+	}
 
-	return tree;
-};
+	_selected = (e) => {
+		console.log('value: ' + e.target.value);
+		console.log('index: ' + e.target.key);
 
-function findFolder(array, key, value) {
-	var t = 0;
-	while (t < array.length && array[t][key] !== value) { t++; };
+		this.setState({
+			selectedValue: e.target.value,
+		});
+	}
 
-	if (t < array.length) {
-		return array[t]
-	} else {
-		return false;
+	render() {
+		return (
+			<StaticQuery
+				query={graphql`
+					query SearchIndexQuery {
+						allMarkdownRemark(filter: { fields: { slug: { regex: "//i" } } }) {
+							edges {
+								node {
+									fields {
+										slug
+									}
+									frontmatter {
+										title
+										description
+									}
+									excerpt
+								}
+							}
+						}
+					}
+				`}
+				render={data => {
+					let navbarClasses = 'sidebar-toggler-content sidenav-fixed sidenav-menu-slider mt-5';
+					if (this.props.navbarToggled) {
+						navbarClasses += ' toggler-expanded';
+					}
+
+					return (
+						<nav className={navbarClasses} id="clay-sidebar">
+							<div className={`${styles.sideBarBody} sidebar-body mb-auto mt-5`}>
+								<Sidebarselect selectItems={this._getTree(data.allMarkdownRemark.edges)} handleSelect={this._selected} />
+
+								<Navigation sectionList={this._getTree(data.allMarkdownRemark.edges)} location={this.props.location} />
+							</div>
+						</nav>
+					)}
+				}
+			/>
+		);
 	}
 }
 
-export default (props) => (
-	<StaticQuery
-		query={graphql`
-			query SearchIndexQuery {
-				allMarkdownRemark(filter: { fields: { slug: { regex: "//i" } } }) {
-					edges {
-						node {
-							fields {
-								slug
-							}
-							frontmatter {
-								title
-								description
-							}
-							excerpt
-						}
-					}
-				}
-			}
-		`}
-		render={data => {
 
-			let navbarClasses = 'sidebar-toggler-content sidenav-fixed sidenav-menu-slider mt-5';
-			if (props.navbarToggled) {
-				navbarClasses += ' toggler-expanded';
-			}
-
-			const selected = (e) => {
-				console.log('value: ' + e.target.value);
-
-				// this.setState({
-				// 	selectedValue: e.target.value,
-				// });
-			}
-
-			return (
-				<nav className={navbarClasses} id="clay-sidebar">
-					<div className={`${styles.sideBarBody} sidebar-body mb-auto mt-5`}>
-						<Sidebarselect selectItems={getTree(data.allMarkdownRemark.edges)} handleSelect={selected} />
-
-						<Navigation sectionList={getTree(data.allMarkdownRemark.edges)} location={props.location} />
-					</div>
-				</nav>
-			)}
-		}
-	/>
-);
+export default Sidebar;
