4eead2c382af8924155aca37be1ec8b643d51d67
Justin Sun-ho Kim <justin.kim@liferay.com>
2019-11-22 14:15:44 -0800

    LWM-2078 Use Blueprints accordion sidebar menu (simpler)

--- src/components/organisms/Sidebar/SidebarSelect.js
+++ src/components/organisms/Sidebar/SidebarSelect.js
@@ -1,7 +1,8 @@
 import React from 'react';
+import styles from './styles.module.scss'
 
 const SidebarSelect = ({ selectItems, handleSelected }) => (
-	<select onChange={handleSelected}>
+	<select className={styles.sidebarSelect} onChange={handleSelected}>
 		{
 			selectItems.map((item, index) => (
 				<option key={index} value={index}>{item}</option>	
