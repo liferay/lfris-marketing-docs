eaa61b5b7ac94a430bcb13b85b7b3fccc9d5ad5d
Justin Sun-ho Kim <justin.kim@liferay.com>
2019-11-22 14:36:45 -0800

    LWM-2078 Add Icons and clean up UX for sidebar

---
+++ src/components/atoms/Icon/index.js
@@ -0,0 +1,53 @@
+import PropTypes from 'prop-types'
+import React from 'react'
+import IconsObject from './icons'
+
+const Icon = ({
+	className,
+	fill,
+	height,
+	width,
+	name,
+	style,
+	margin,
+	rotate,
+	...props
+}) => {
+	const styles = {
+		...style,
+		fill: '#09101D',
+		height,
+		margin,
+		width,
+	}
+
+	if (rotate) {
+		styles.transform = `rotate(${rotate}deg)`
+		styles.transition = 'transform 0.5s'
+	}
+
+	const DynamicIcon = IconsObject[name]
+
+	if (!DynamicIcon) {
+		console.log(`${name} icon not found`)
+	}
+
+	return DynamicIcon ? (
+		<DynamicIcon className={className} style={styles} name={name} {...props} />
+	) : (
+		<span />
+	)
+}
+
+Icon.propTypes = {
+	className: PropTypes.string,
+	fill: PropTypes.string,
+	height: PropTypes.string,
+	width: PropTypes.string,
+	name: PropTypes.string.isRequired,
+	style: PropTypes.object,
+	margin: PropTypes.string,
+	rotate: PropTypes.number,
+}
+
+export default Icon
