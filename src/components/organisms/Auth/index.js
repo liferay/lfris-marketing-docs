edebe21e015bf96dc5f5851a64d3bd7e60dfb3de
Justin Sun-ho Kim <justin.kim@liferay.com>
2019-12-02 16:23:20 -0800

    LWM-2076 Style Gated Page

--- src/components/organisms/Auth/index.js
+++ src/components/organisms/Auth/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { useIdentityContext } from 'react-netlify-identity-widget'
 import styles from './styles.module.scss'
+import { Login } from 'components/molecules'
 
 const Auth = ({ children, needsAuth }) => {
 	const identity = useIdentityContext();
@@ -9,13 +10,12 @@ const Auth = ({ children, needsAuth }) => {
 		<>
 			{!needsAuth || (identity && identity.isLoggedIn && identity.user.email.includes('@liferay.com')) ? 
 				children :
-				`Please sign in with a @liferay.com email address`
-			}
-			{
-				console.log(needsAuth)
-			}
-			{
-				console.log(identity)
+				<div className={`row ${styles.gateStyles}`}>
+					<div className={`max-width-full margin-horizontal-auto ${styles.gateNotice}`}>
+						Please sign in with a @liferay.com email address to view this content
+						<Login className={styles.loginButton} />
+					</div>
+				</div>
 			}
 		</>
 	);
